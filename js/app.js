04bcc86703988a101b6b1a1d8ccef0467f5d91b4
Enmanuel Ogando <76906650+enmanuel0112@users.noreply.github.com>
2023-02-05 15:44:03 -0400

    new version updated

--- js/app.js
+++ js/app.js
@@ -3,12 +3,141 @@ window.addEventListener('scroll', function(){
     header.classList.toggle('down',window.scrollY> 0);
 });
 
-const navToggle = document.querySelector('.nav-toggle');
+/**Menu */
+
+const navToggle = document.querySelector('.show-menu');
 const navMenu = document.querySelector('.navegation');
 
 navToggle.addEventListener('click', () =>{
     navMenu.classList.toggle('active');
 });
+
+
+
+/**Car Shop */
+const menuContent = document.getElementById('menuContianer');
+const shopCar = document.getElementById('shop-car');
+const modalContainer = document.getElementById('modal-container');
+
+
+let car = [];
+
+
+products.forEach((product) =>{
+
+  let contents = document.createElement('div'); 
+  contents.className = 'card';
+
+  contents.innerHTML =  `
+  <img src="${product.img}"> 
+  <h3> ${product.name}</h3>
+  <p> ${`Price :`} ${product.price} $ </p>
+  `;
+
+  menuContent.append(contents);
+
+  let buy = document.createElement('button');
+
+  buy.innerText = 'Buy';
+  buy.className = 'btn-menuContent';
+  
+  contents.append(buy);
+
+
+
+  buy.addEventListener('click', () =>{
+
+    const repeat = car.some((repeatProduct) =>  repeatProduct.id === product.id);
+    
+    if(repeat){
+      car.map((prod) =>{
+        if(prod.id === product.id){
+          prod.amount ++;
+        }
+      })
+    }else{
+      car.push({
+        id: product.id,
+        img: product.img,
+        name: product.name,
+        price: product.price,
+        amount: product.amount
+  
+      });
+    }
+
+
+  });
+});
+
+
+
+  const showCar = () => {
+
+  const modalCar = document.createElement('div');
+  modalCar.className = 'modal-car';
+  modalCar.innerHTML = `
+  <h2 class="modal-car-tittle">Shop</h2>
+  `;
+  modalContainer.style.display = 'block'
+  modalContainer.innerHTML = "";
+
+  modalContainer.append(modalCar);
+
+   /**Modal Button */
+  const modalButton = document.createElement('button');
+  modalButton.innerText = 'X';
+  modalButton.className = 'modal-button';
+
+  modalButton.addEventListener('click', () => modalContainer.style.display = 'none' ) ;
+  modalCar.append(modalButton); 
+
+
+  /**Modal Container */
+
+car.forEach((product) =>{
+ 
+  let carContent = document.createElement('div');
+  carContent.className = 'modal-content';
+  carContent.innerHTML =`
+  <img src="${product.img}" class='prueba'>
+  <h3>${product.name}</h3>
+  <p> ${product.price} $ </p>
+  <p> Amount: ${product.amount} $ </p>
+
+  `;
+
+  modalContainer.append(carContent);
+
+  let Delete = document.createElement('span');
+  Delete.innerText = 'X';
+  Delete.className = 'btn-delete';
+  
+  carContent.append(Delete);
+
+  Delete.addEventListener('click', () => deleteProduct())
+});
+  
+/**Modal Total */
+const total = car.reduce((acc, the) => acc + the.price * the.amount, 0 );
+
+const totalPay = document.createElement('div');
+totalPay.className = 'total-paying';
+totalPay.innerHTML = `
+Total: ${total} $
+`;
+modalContainer.append(totalPay);
+}
+
+shopCar.addEventListener('click', () => showCar());
+const deleteProduct = () =>{
+  const foundId = car.find((element) => element.id);
+
+  car = car.filter((carId) => carId !== foundId);
+
+
+  showCar();
+}
 
 
 /**Testimonial */
---
+++ js/product.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+      id: 1,
+      name: 'Doble Burger',
+      price: 7,
+      img: 'img/burger1.png',
+      amount: 1,
+    },
+  
+    {
+      id: 2,
+      name: 'Great Chesse',
+      price:  8.5,
+      img: 'img/burger2.png',
+      amount: 1,
+    },
+    {
+      id: 3,
+      name: 'Doble Meat',
+      price: 10,
+      img: 'img/burger3.png',
+      amount: 1,
+    },
+    {
+      id: 4,
+      name: 'Big Burger',
+      price: 9,
+      img: 'img/burger4.png',
+      amount: 1,
+    },
+    {
+      id: 5,
+      name: 'Nice Burger',
+      price: 6,
+      img: 'img/burger5.png',
+      amount: 1,
+    },
+    {
+      id: 6,
+      name: 'veggie Burger',
+      price: 5.50,
+      img: 'img/burger6.png',
+      amount: 1,
+    },
+    {
+      id: 7,
+      name: 'Chicken Burger',
+      price: 8,
+      img: 'img/burger7.png',
+      amount: 1,
+    },
+    {
+      id: 8,
+      name: 'Chruss Burger',
+      price: 6,
+      img: 'img/burger8.png',
+      amount: 1,
+    }, 
+     {
+      id: 9,
+      name: 'Fish Burger',
+      price: 9,
+      img: 'img/burger9.png',
+      amount: 1,
+    },
+  
+  
+  ];
